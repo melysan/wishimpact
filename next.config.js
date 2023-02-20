de3a123742575cc3b9d434498c6e8b420e596f43
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-02-20 00:21:43 -0800

    add genshin data

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
+  images: {
+    domains: ["static.wikia.nocookie.net"]
+  }
 }
 
 module.exports = nextConfig
