7fb899b5d33823d666070e0fe00d3f148e3c8ca5
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-02-20 14:47:00 -0800

    footer and styling

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import styles from '@/styles/Footer.module.css'
+import Link from 'next/link'
+
+
+export function Footer() {
+    return (
+        <>
+            <div className={styles.footerCont}>
+                <div className={styles.footerText}>© 2023 Melysa Nguyen</div>
+            </div>
+        </>
+    )
+}
+
+
