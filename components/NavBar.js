e90c27f38dda0535041ffc9848dfa5d37c35812e
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-02-20 17:25:11 -0800

    navbar change

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,6 @@
 import styles from '@/styles/NavBar.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 export function NavBar() {
@@ -8,6 +9,7 @@ export function NavBar() {
             <ul className={styles.navBarCont}>
                 <Link className={styles.navLink} href="/characters">View Characters</Link>
                 <Link className={styles.navLink} href="/">Home</Link>
+                <Image className={styles.navIcon} src={"Wish_Impact_Icon.svg"} width={50} height={50} />
             </ul>
         </>
     )
